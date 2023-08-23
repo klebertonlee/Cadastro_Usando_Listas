d902c28d31e9b6a485ef7cbd06fbf6f13e0dc5b4
Kleberton Leandro <kpleandro@hotmail.com>
2023-08-23 11:12:36 -0300

    lista Add e Remove

---
+++ Exercicio10/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Net.WebSockets;
+
+
+namespace Exercicio10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("         ----------  KLEBERTON TECH  ----------");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            List<TesteList> list = new List<TesteList>();
+
+            Console.Write("Quantos tipos de produtos serão armazenados: ");
+            int Tipos = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Digite os códigos do produto:");
+            Console.WriteLine();
+
+
+            for (int i = 1; i <= Tipos; i++)
+            {
+                Console.Write("Código: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Nome do Produto: ");
+                string nome = Console.ReadLine();
+                Console.Write("Valor de custo de Compra do Produto: $");
+                double valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Quantidade do produto: ");
+                int quantidade = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                list.Add(new TesteList(id, nome, valor, quantidade));              
+            }
+
+            Console.WriteLine("Relatorio de item cadastrados:");
+            foreach (TesteList item in list)
+            {
+                Console.WriteLine();
+                Console.WriteLine(item);
+            }
+
+            
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Exercicio10/TesteList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Exercicio10
+{
+    internal class TesteList
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Valor { get; set; }
+        public int Quantidade { get; set; }
+
+        public TesteList(int id, string nome, double valor, int quantidade)
+        {
+            Id = id;
+            Nome = nome;
+            Valor = valor;
+            Quantidade = quantidade;
+        }
+
+        public double ValorQuantidade()
+        {
+            return Valor * Quantidade;
+        }
+
+        public override string ToString()
+        {
+            return "Código: "
+                + Id
+                + ", Produto: "
+                + Nome.ToUpper()
+                + ", Valor: "
+                + Valor.ToString("F2", CultureInfo.InvariantCulture)
+                + ", Quantidade Armazenada: "
+                + Quantidade
+                + ", Valor total da Compra: $"
+                + ValorQuantidade().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
